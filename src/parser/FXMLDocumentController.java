1eff86487cdf6203b82f739b0e0b2666980feab4
kilian <kilian@kk>
2016-01-31 15:04:37 +0100

    cleaning up (file bugfix)

---
+++ src/parser/FXMLDocumentController.java
@@ -0,0 +1,33 @@
+package parser;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class FXMLDocumentController implements Initializable {
+
+    @FXML
+    private TextField tbInput;
+    @FXML
+    private Button btnSolve;
+    @FXML
+    private Label lblSolution;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+
+    }
+
+    @FXML
+    private void solve(ActionEvent event) {
+        
+        
+        
+    }
+
+}
